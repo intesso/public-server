6148039087544b8517e32513c603abdecac7b182
andi-neck <andi.neck@intesso.com>
2014-08-26 08:17:51 +0200

    cleanup before merge with `hidden-server`

--- index.js
+++
@@ -1,9 +0,0 @@
-var types = {
-  'hidden': './lib/hidden.js',
-  'public': './lib/public.js'
-};
-
-module.exports = function (type) {
-  if (!types[type]) throw new Error('type must be "hidden" or "public"');
-  return require(types[type]);
-}
